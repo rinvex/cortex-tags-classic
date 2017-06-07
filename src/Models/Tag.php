0a5b6f5ac55f8f6678bbc0dff35ccebe787d7759
Abdelrahman Omran <me@omranic.com>
2017-06-07 13:04:16 +0200

    Add tag model docblock

--- src/Models/Tag.php
+++ src/Models/Tag.php
@@ -6,6 +6,31 @@ namespace Cortex\Taggable\Models;
 
 use Rinvex\Taggable\Tag as BaseTag;
 
+/**
+ * Cortex\Taggable\Models\Tag
+ *
+ * @property int            $id
+ * @property string         $slug
+ * @property array          $name
+ * @property array          $description
+ * @property int            $sort_order
+ * @property string         $group
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @property string         $deleted_at
+ * @method static \Illuminate\Database\Query\Builder|\Rinvex\Taggable\Tag ordered($direction = 'asc')
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Taggable\Models\Tag whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Taggable\Models\Tag whereDeletedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Taggable\Models\Tag whereDescription($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Taggable\Models\Tag whereGroup($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Taggable\Models\Tag whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Taggable\Models\Tag whereName($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Taggable\Models\Tag whereSlug($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Taggable\Models\Tag whereSortOrder($value)
+ * @method static \Illuminate\Database\Query\Builder|\Cortex\Taggable\Models\Tag whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\Rinvex\Taggable\Tag withGroup($group = null)
+ * @mixin \Eloquent
+ */
 class Tag extends BaseTag
 {
     //
