c5879e370e2bef88d2c6a401119a18fef8ac86d2
Abdelrahman Omran <me@omranic.com>
2017-06-19 15:37:35 +0200

    Update model class docblock

--- src/Models/Tag.php
+++ src/Models/Tag.php
@@ -9,27 +9,27 @@ use Rinvex\Taggable\Tag as BaseTag;
 /**
  * Cortex\Taggable\Models\Tag.
  *
- * @property int            $id
- * @property string         $slug
- * @property array          $name
- * @property array          $description
- * @property int            $sort_order
- * @property string         $group
- * @property \Carbon\Carbon $created_at
- * @property \Carbon\Carbon $updated_at
- * @property string         $deleted_at
+ * @property int                 $id
+ * @property string              $slug
+ * @property array               $name
+ * @property array               $description
+ * @property int                 $sort_order
+ * @property string|null         $group
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property \Carbon\Carbon|null $deleted_at
  *
- * @method static \Illuminate\Database\Query\Builder|\Rinvex\Taggable\Tag ordered($direction = 'asc')
- * @method static \Illuminate\Database\Query\Builder|\Cortex\Taggable\Models\Tag whereCreatedAt($value)
- * @method static \Illuminate\Database\Query\Builder|\Cortex\Taggable\Models\Tag whereDeletedAt($value)
- * @method static \Illuminate\Database\Query\Builder|\Cortex\Taggable\Models\Tag whereDescription($value)
- * @method static \Illuminate\Database\Query\Builder|\Cortex\Taggable\Models\Tag whereGroup($value)
- * @method static \Illuminate\Database\Query\Builder|\Cortex\Taggable\Models\Tag whereId($value)
- * @method static \Illuminate\Database\Query\Builder|\Cortex\Taggable\Models\Tag whereName($value)
- * @method static \Illuminate\Database\Query\Builder|\Cortex\Taggable\Models\Tag whereSlug($value)
- * @method static \Illuminate\Database\Query\Builder|\Cortex\Taggable\Models\Tag whereSortOrder($value)
- * @method static \Illuminate\Database\Query\Builder|\Cortex\Taggable\Models\Tag whereUpdatedAt($value)
- * @method static \Illuminate\Database\Query\Builder|\Rinvex\Taggable\Tag withGroup($group = null)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Rinvex\Taggable\Tag ordered($direction = 'asc')
+ * @method static \Illuminate\Database\Eloquent\Builder|\Cortex\Taggable\Models\Tag whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Cortex\Taggable\Models\Tag whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Cortex\Taggable\Models\Tag whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Cortex\Taggable\Models\Tag whereGroup($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Cortex\Taggable\Models\Tag whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Cortex\Taggable\Models\Tag whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Cortex\Taggable\Models\Tag whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Cortex\Taggable\Models\Tag whereSortOrder($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Cortex\Taggable\Models\Tag whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Rinvex\Taggable\Tag withGroup($group = null)
  * @mixin \Eloquent
  */
 class Tag extends BaseTag
