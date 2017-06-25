07f133fe1d60f4ab650e157517699cbe748698f3
Abdelrahman Omran <me@omranic.com>
2017-06-25 07:41:10 +0200

    Group tags in datatables

--- src/DataTables/Backend/TagsDataTable.php
+++ src/DataTables/Backend/TagsDataTable.php
@@ -21,6 +21,66 @@ class TagsDataTable extends AbstractDataTable
     protected $transformer = TagTransformer::class;
 
     /**
+     * Get the query object to be processed by dataTables.
+     *
+     * @return \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Query\Builder|\Illuminate\Support\Collection
+     */
+    public function query()
+    {
+        $query = ($this->model)::query()->orderBy('group', 'ASC');
+
+        return $this->applyScopes($query);
+    }
+
+    /**
+     * Get parameters.
+     *
+     * @return array
+     */
+    protected function getParameters()
+    {
+        return [
+            'keys' => true,
+            'autoWidth' => false,
+            'dom' => "<'row'<'col-sm-6'B><'col-sm-6'f>> <'row'r><'row'<'col-sm-12't>> <'row'<'col-sm-5'i><'col-sm-7'p>>",
+            'buttons' => [
+                ['extend' => 'create', 'text' => '<i class="fa fa-plus"></i> '.trans('cortex/foundation::common.new')], 'print', 'reset', 'reload', 'export',
+                ['extend' => 'colvis', 'text' => '<i class="fa fa-columns"></i> '.trans('cortex/foundation::common.columns').' <span class="caret"/>'],
+            ],
+            'drawCallback' => 'function (settings) {
+                var lastGroup = null;
+                var api = this.api();
+                var colspan = api.columns(\':visible\').count();
+                var rows = api.rows({page:\'current\'}).nodes();
+
+                api.column(\'group:name\', {page:\'current\'} ).data().each(function (rowGroup, rowIndex) {
+                    if (lastGroup !== rowGroup) {
+                        $(rows).eq(rowIndex).before(
+                            \'<tr class="tag-group"><td colspan="\'+colspan+\'"><strong>\'+(rowGroup ? rowGroup : "No Group")+\'</strong></td></tr>\'
+                        );
+     
+                        lastGroup = rowGroup;
+                    }
+                });
+            }',
+        ];
+    }
+
+    /**
+     * Display ajax response.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function ajax()
+    {
+        return $this->datatables
+            ->eloquent($this->query())
+            ->setTransformer(new $this->transformer)
+            ->orderColumn('name', 'name->"$.'.app()->getLocale().'" $1')
+            ->make(true);
+    }
+
+    /**
      * Get columns.
      *
      * @return array
@@ -30,6 +90,7 @@ class TagsDataTable extends AbstractDataTable
         return [
             'name' => ['title' => trans('cortex/taggable::common.name'), 'render' => '"<a href=\""+routes.route(\'backend.tags.edit\', {tag: full.id})+"\">"+data+"</a>"', 'responsivePriority' => 0],
             'slug' => ['title' => trans('cortex/taggable::common.slug')],
+            'group' => ['title' => trans('cortex/taggable::common.group'), 'visible' => false],
             'created_at' => ['title' => trans('cortex/taggable::common.created_at'), 'render' => "moment(data).format('MMM Do, YYYY')"],
             'updated_at' => ['title' => trans('cortex/taggable::common.updated_at'), 'render' => "moment(data).format('MMM Do, YYYY')"],
         ];
--- src/Transformers/Backend/TagTransformer.php
+++ src/Transformers/Backend/TagTransformer.php
@@ -18,6 +18,8 @@ class TagTransformer extends TransformerAbstract
             'id' => (int) $tag->id,
             'name' => (string) $tag->name,
             'slug' => (string) $tag->slug,
+            'group' => (string) $tag->group,
+            'sort_order' => (string) $tag->sort_order,
             'created_at' => (string) $tag->created_at,
             'updated_at' => (string) $tag->updated_at,
         ];
