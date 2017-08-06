37acc4d2e3b29498ad560a7159a5f6e6a8a98071
Abdelrahman Omran <me@omranic.com>
2017-08-06 17:19:01 +0200

    Remove useless namespace usage

--- src/Providers/TaggableServiceProvider.php
+++ src/Providers/TaggableServiceProvider.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Cortex\Taggable\Providers;
 
-use Illuminate\Routing\Router;
 use Illuminate\Support\ServiceProvider;
 
 class TaggableServiceProvider extends ServiceProvider
