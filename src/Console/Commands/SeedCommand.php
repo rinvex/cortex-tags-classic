ad258f1cfb532b8179846440a1740bbf1ac515b9
Abdelrahman Omran <me@omranic.com>
2017-08-18 22:44:30 +0200

    Tweak seeder's output

--- src/Console/Commands/SeedCommand.php
+++ src/Console/Commands/SeedCommand.php
@@ -35,6 +35,8 @@ class SeedCommand extends Command
      */
     public function handle()
     {
+        $this->warn('Seed cortex/taggable:');
+
         if ($this->ensureExistingTaggableTables()) {
             // No seed data at the moment!
         }
