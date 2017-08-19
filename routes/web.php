d1a7bcb6d46b5db57011554c81d30b48eb9bb410
Abdelrahman Omran <me@omranic.com>
2017-08-19 06:10:15 +0200

    Rename core cortex config namespace

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ Route::group(['domain' => domain()], function () {
     Route::name('backend.')
          ->namespace('Cortex\Taggable\Http\Controllers\Backend')
          ->middleware(['web', 'nohttpcache', 'can:access-dashboard'])
-         ->prefix(config('rinvex.cortex.route.locale_prefix') ? '{locale}/backend' : 'backend')->group(function () {
+         ->prefix(config('cortex.foundation.route.locale_prefix') ? '{locale}/backend' : 'backend')->group(function () {
 
         // Tags Routes
         Route::name('tags.')->prefix('tags')->group(function () {
