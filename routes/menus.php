114d7d114c1ecb3be5872347497caa035c0c31e0
Abdelrahman Omran <me@omranic.com>
2017-08-20 03:07:17 +0200

    Change backend sidebar menu section registration

--- routes/menus.php
+++ routes/menus.php
@@ -2,4 +2,4 @@
 
 declare(strict_types=1);
 
-Menu::backendSidebar('resources')->routeIfCan('list-tags', 'backend.tags.index', '<i class="fa fa-tags"></i> <span>'.trans('cortex/taggable::common.tags').'</span>');
+Menu::backendSidebar('taxonomies')->routeIfCan('list-tags', 'backend.tags.index', '<i class="fa fa-tags"></i> <span>'.trans('cortex/taggable::common.tags').'</span>');
