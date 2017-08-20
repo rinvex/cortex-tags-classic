78ec00c7c408a1a8b2c2a0bf3619f3ee778b0dcd
Abdelrahman Omran <me@omranic.com>
2017-08-20 21:05:03 +0200

    Refactor SeedCommand to use the new SeederHelper

--- src/Console/Commands/SeedCommand.php
+++ src/Console/Commands/SeedCommand.php
@@ -5,14 +5,11 @@ declare(strict_types=1);
 namespace Cortex\Taggable\Console\Commands;
 
 use Illuminate\Console\Command;
-use Rinvex\Fort\Traits\AbilitySeeder;
-use Rinvex\Fort\Traits\ArtisanHelper;
-use Illuminate\Support\Facades\Schema;
+use Rinvex\Support\Traits\SeederHelper;
 
 class SeedCommand extends Command
 {
-    use AbilitySeeder;
-    use ArtisanHelper;
+    use SeederHelper;
 
     /**
      * The name and signature of the console command.
@@ -37,37 +34,8 @@ class SeedCommand extends Command
     {
         $this->warn('Seed cortex/taggable:');
 
-        if ($this->ensureExistingTaggableTables()) {
-            // No seed data at the moment!
+        if ($this->ensureExistingDatabaseTables('rinvex/fort')) {
+            $this->seedResources(app('rinvex.fort.ability'), realpath(__DIR__.'/../../../resources/data/abilities.json'), ['name', 'description']);
         }
-
-        if ($this->ensureExistingFortTables()) {
-            $this->seedAbilities(realpath(__DIR__.'/../../../resources/data/abilities.json'));
-        }
-    }
-
-    /**
-     * Ensure existing taggable tables.
-     *
-     * @return bool
-     */
-    protected function ensureExistingTaggableTables()
-    {
-        if (! $this->hasTaggableTables()) {
-            $this->call('cortex:migrate:taggable');
-        }
-
-        return true;
-    }
-
-    /**
-     * Check if all required taggable tables exists.
-     *
-     * @return bool
-     */
-    protected function hasTaggableTables()
-    {
-        return Schema::hasTable(config('rinvex.taggable.tables.tags'))
-               && Schema::hasTable(config('rinvex.taggable.tables.taggables'));
     }
 }
