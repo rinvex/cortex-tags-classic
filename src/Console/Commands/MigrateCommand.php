fdf2e4b9182dcb1871463431db82eb564c2d2009
Abdelrahman Omran <me@omranic.com>
2017-10-30 00:34:28 +0200

    Add missing module migration execution step

--- src/Console/Commands/MigrateCommand.php
+++ src/Console/Commands/MigrateCommand.php
@@ -21,4 +21,16 @@ class MigrateCommand extends BaseMigrateCommand
      * @var string
      */
     protected $description = 'Migrate Cortex Tags Tables.';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        parent::handle();
+
+        $this->call('migrate', ['--step' => true, '--path' => 'app/cortex/tags/database/migrations']);
+    }
 }
