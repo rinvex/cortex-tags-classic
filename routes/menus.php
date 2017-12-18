07e08488dcd9f452867b15f67435b403cf02de46
Abdelrahman Omran <me@omranic.com>
2017-12-18 16:20:42 +0200

    Refactor menus

--- routes/menus.php
+++ routes/menus.php
@@ -2,4 +2,12 @@
 
 declare(strict_types=1);
 
-Menu::adminareaSidebar('taxonomies')->routeIfCan('list-tags', 'adminarea.tags.index', '<i class="fa fa-tags"></i> <span>'.trans('cortex/tags::common.tags').'</span>');
+use Rinvex\Menus\Models\MenuItem;
+use Rinvex\Menus\Factories\MenuFactory;
+
+Menu::modify('adminarea.sidebar', function(MenuFactory $menu) {
+    $menu->findBy('title', trans('cortex/foundation::common.taxonomy'), function (MenuItem $dropdown) {
+        $dropdown->route(['adminarea.tags.index'], trans('cortex/tags::common.tags'), 20, 'fa fa-tags')->can('list-tags');
+    });
+});
+
