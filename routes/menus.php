a38b40039264d93013b7bdefedd69fcf440921b0
Abdelrahman Omran <me@omranic.com>
2017-12-19 17:43:03 +0200

    Tweak menus

--- routes/menus.php
+++ routes/menus.php
@@ -6,7 +6,7 @@ use Rinvex\Menus\Models\MenuItem;
 use Rinvex\Menus\Factories\MenuFactory;
 
 Menu::modify('adminarea.sidebar', function(MenuFactory $menu) {
-    $menu->findBy('title', trans('cortex/foundation::common.taxonomy'), function (MenuItem $dropdown) {
+    $menu->findByTitleOrAdd(trans('cortex/foundation::common.taxonomy'), 10, 'fa fa-arrows', [], function (MenuItem $dropdown) {
         $dropdown->route(['adminarea.tags.index'], trans('cortex/tags::common.tags'), 20, 'fa fa-tags')->can('list-tags');
     });
 });
