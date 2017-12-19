9ccd79e959ad5238fad55024df9588f764ad0c6c
Abdelrahman Omran <me@omranic.com>
2017-12-19 15:44:36 +0000

    Apply fixes from StyleCI

--- routes/menus.php
+++ routes/menus.php
@@ -5,9 +5,8 @@ declare(strict_types=1);
 use Rinvex\Menus\Models\MenuItem;
 use Rinvex\Menus\Factories\MenuFactory;
 
-Menu::modify('adminarea.sidebar', function(MenuFactory $menu) {
+Menu::modify('adminarea.sidebar', function (MenuFactory $menu) {
     $menu->findByTitleOrAdd(trans('cortex/foundation::common.taxonomy'), 10, 'fa fa-arrows', [], function (MenuItem $dropdown) {
         $dropdown->route(['adminarea.tags.index'], trans('cortex/tags::common.tags'), 20, 'fa fa-tags')->can('list-tags');
     });
 });
-
