edc336448917399840f0f415cc6bca0d69cffe39
Abdelrahman Omran <me@omranic.com>
2018-01-02 03:18:10 +0200

    Simplify & highlight adminarea/managerarea breadcrumbs

--- resources/views/adminarea/pages/tag.blade.php
+++ resources/views/adminarea/pages/tag.blade.php
@@ -19,9 +19,7 @@
 
     <div class="content-wrapper">
         <section class="content-header">
-            <h1>{{ $tag->exists ? $tag->name : trans('cortex/tags::common.create_tag') }}</h1>
-            <!-- Breadcrumbs -->
-            {{ Breadcrumbs::render() }}
+            <h1>{{ Breadcrumbs::render() }}</h1>
         </section>
 
         <!-- Main content -->
