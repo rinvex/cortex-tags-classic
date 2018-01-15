490297ba3aa49ade1354d073ce3808d314a3a85e
Abdelrahman Omran <me@omranic.com>
2018-01-15 12:18:04 +0200

    Refactor delete confirmation & unify it across the whole project

--- resources/views/adminarea/pages/tag.blade.php
+++ resources/views/adminarea/pages/tag.blade.php
@@ -14,7 +14,7 @@
 @section('content')
 
     @if($tag->exists)
-        @include('cortex/foundation::common.partials.confirm-deletion', ['type' => 'tag'])
+        @include('cortex/foundation::common.partials.confirm-deletion')
     @endif
 
     <div class="content-wrapper">
@@ -29,7 +29,7 @@
                 <ul class="nav nav-tabs">
                     <li class="active"><a href="#details-tab" data-toggle="tab">{{ trans('cortex/tags::common.details') }}</a></li>
                     @if($tag->exists) <li><a href="#logs-tab" data-toggle="tab">{{ trans('cortex/tags::common.logs') }}</a></li> @endif
-                    @if($tag->exists && $currentUser->can('delete-tags', $tag)) <li class="pull-right"><a href="#" data-toggle="modal" data-target="#delete-confirmation" data-item-href="{{ route('adminarea.tags.delete', ['tag' => $tag]) }}" data-item-name="{{ $tag->slug }}"><i class="fa fa-trash text-danger"></i></a></li> @endif
+                    @if($tag->exists && $currentUser->can('delete-tags', $tag)) <li class="pull-right"><a href="#" data-toggle="modal" data-target="#delete-confirmation" data-modal-action="{{ route('adminarea.tags.delete', ['tag' => $tag]) }}" data-modal-title="{!! trans('cortex/foundation::messages.delete_confirmation_title') !!}" data-modal-body="{!! trans('cortex/foundation::messages.delete_confirmation_body', ['type' => 'tag', 'name' => $tag->slug]) !!}" title="{{ trans('cortex/foundation::common.delete') }}"><i class="fa fa-trash text-danger"></i></a></li> @endif
                 </ul>
 
                 <div class="tab-content">
