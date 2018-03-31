8665c6804b26a693d6838a2365c5c5cd91d2713d
Abdelrahman Omran <me@omranic.com>
2018-03-31 19:14:32 +0200

    Enforce consistency

--- src/Http/Controllers/Adminarea/TagsController.php
+++ src/Http/Controllers/Adminarea/TagsController.php
@@ -183,7 +183,7 @@ class TagsController extends AuthorizedController
 
         return intend([
             'url' => route('adminarea.tags.index'),
-            'with' => ['success' => trans('cortex/foundation::messages.resource_saved', ['resource' => 'tag', 'identifier' => $tag->slug])],
+            'with' => ['success' => trans('cortex/foundation::messages.resource_saved', ['resource' => 'tag', 'identifier' => $tag->name])],
         ]);
     }
 
@@ -200,7 +200,7 @@ class TagsController extends AuthorizedController
 
         return intend([
             'url' => route('adminarea.tags.index'),
-            'with' => ['warning' => trans('cortex/foundation::messages.resource_deleted', ['resource' => 'tag', 'identifier' => $tag->slug])],
+            'with' => ['warning' => trans('cortex/foundation::messages.resource_deleted', ['resource' => 'tag', 'identifier' => $tag->name])],
         ]);
     }
 }
