bf5c3aa6f40b11cc4a60b7b8943f401a7e74e4fb
Abdelrahman Omran <me@omranic.com>
2018-03-31 22:35:59 +0200

    Escape datatable rows

--- src/Transformers/Adminarea/TagTransformer.php
+++ src/Transformers/Adminarea/TagTransformer.php
@@ -5,21 +5,24 @@ declare(strict_types=1);
 namespace Cortex\Tags\Transformers\Adminarea;
 
 use Cortex\Tags\Models\Tag;
+use Rinvex\Support\Traits\Escaper;
 use League\Fractal\TransformerAbstract;
 
 class TagTransformer extends TransformerAbstract
 {
+    use Escaper;
+
     /**
      * @return array
      */
     public function transform(Tag $tag): array
     {
-        return [
+        return $this->escapeRow([
             'id' => (string) $tag->getRouteKey(),
             'name' => (string) $tag->name,
             'group' => (string) $tag->group,
             'created_at' => (string) $tag->created_at,
             'updated_at' => (string) $tag->updated_at,
-        ];
+        ]);
     }
 }
