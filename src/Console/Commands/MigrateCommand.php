e577d05773fe5329fa6b3306ab0acd676e79fd53
Abdelrahman Omran <me@omranic.com>
2020-03-18 15:18:11 +0400

    Fix migrations path condition

--- src/Console/Commands/MigrateCommand.php
+++ src/Console/Commands/MigrateCommand.php
@@ -31,7 +31,11 @@ class MigrateCommand extends BaseMigrateCommand
     {
         parent::handle();
 
-        if (file_exists($path = 'database/migrations/cortex/tags')) {
+        $path = config('cortex.tags.autoload_migrations') ?
+            'app/cortex/tags/database/migrations' :
+            'database/migrations/cortex/tags';
+
+        if (file_exists($path)) {
             $this->call('migrate', [
                 '--step' => true,
                 '--path' => $path,
--- src/Console/Commands/RollbackCommand.php
+++ src/Console/Commands/RollbackCommand.php
@@ -29,7 +29,11 @@ class RollbackCommand extends BaseRollbackCommand
      */
     public function handle(): void
     {
-        if (file_exists($path = 'database/migrations/cortex/tags')) {
+        $path = config('cortex.tags.autoload_migrations') ?
+            'app/cortex/tags/database/migrations' :
+            'database/migrations/cortex/tags';
+
+        if (file_exists($path)) {
             $this->call('migrate:reset', [
                 '--path' => $path,
                 '--force' => $this->option('force'),
