ebf046e1ce175a97d58604a2f483b5872b67aa5b
Abdelrahman Omran <me@omranic.com>
2020-08-03 12:02:28 +0400

    Activate module after installation

--- src/Console/Commands/InstallCommand.php
+++ src/Console/Commands/InstallCommand.php
@@ -34,5 +34,7 @@ class InstallCommand extends Command
         $this->call('cortex:publish:tags', ['--force' => $this->option('force'), '--resource' => $this->option('resource')]);
         $this->call('cortex:migrate:tags', ['--force' => $this->option('force')]);
         $this->call('cortex:seed:tags');
+
+        $this->call('cortex:activate', ['--module' => 'cortex/tags']);
     }
 }
