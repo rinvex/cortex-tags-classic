1517f680d384d994476a71718438a2ab9779b63d
Abdelrahman Omran <me@omranic.com>
2021-08-05 00:13:58 +0400

    Simplify route prefixes

--- routes/web/adminarea.php
+++ routes/web/adminarea.php
@@ -6,7 +6,7 @@ Route::domain(domain())->group(function () {
     Route::name('adminarea.')
          ->namespace('Cortex\Tags\Http\Controllers\Adminarea')
          ->middleware(['web', 'nohttpcache', 'can:access-adminarea'])
-         ->prefix(config('cortex.foundation.route.locale_prefix') ? '{locale}/'.config('cortex.foundation.route.prefix.adminarea') : config('cortex.foundation.route.prefix.adminarea'))->group(function () {
+         ->prefix(route_prefix('adminarea'))->group(function () {
 
         // Tags Routes
              Route::name('cortex.tags.tags.')->prefix('tags')->group(function () {
