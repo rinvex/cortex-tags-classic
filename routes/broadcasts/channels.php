79b199e8ffc95873b5f20bfee2cc5c19b18447ed
Abdelrahman Omran <me@omranic.com>
2022-02-02 10:39:03 +0400

    Fix broadcasts naming convensions

--- routes/broadcasts/channels.php
+++ routes/broadcasts/channels.php
@@ -6,4 +6,4 @@ use Illuminate\Contracts\Auth\Access\Authorizable;
 
 Broadcast::channel('cortex.tags.tags.index', function (Authorizable $user) {
     return $user->can('list', app('rinvex.tags.tag'));
-}, ['guards' => ['admin']]);
+});
