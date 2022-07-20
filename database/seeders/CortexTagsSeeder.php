9a249979e7440cd7587d6e46fbfc5b81d5d6e5e3
Abdelrahman Omran <me@omranic.com>
2022-07-20 23:24:13 +0400

    Add missing export ability

--- database/seeders/CortexTagsSeeder.php
+++ database/seeders/CortexTagsSeeder.php
@@ -18,6 +18,7 @@ class CortexTagsSeeder extends Seeder
         $abilities = [
             ['name' => 'list', 'title' => 'List tags', 'entity_type' => 'tag'],
             ['name' => 'import', 'title' => 'Import tags', 'entity_type' => 'tag'],
+            ['name' => 'export', 'title' => 'Export tags', 'entity_type' => 'tag'],
             ['name' => 'create', 'title' => 'Create tags', 'entity_type' => 'tag'],
             ['name' => 'update', 'title' => 'Update tags', 'entity_type' => 'tag'],
             ['name' => 'delete', 'title' => 'Delete tags', 'entity_type' => 'tag'],
