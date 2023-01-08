769bebc1a9fef154ae54276966ec48c12f152257
Abdelrahman Omran <me@omranic.com>
2023-01-09 02:18:35 +0400

    Move Relation::morphMap to vendor core package

--- bootstrap/module.php
+++ bootstrap/module.php
@@ -2,15 +2,8 @@
 
 declare(strict_types=1);
 
-use Illuminate\Database\Eloquent\Relations\Relation;
-
 return function () {
     // Bind route models and constrains
     Route::pattern('tag', '[a-zA-Z0-9-_]+');
     Route::model('tag', config('rinvex.tags.models.tag'));
-
-    // Map relations
-    Relation::morphMap([
-        'tag' => config('rinvex.tags.models.tag'),
-    ]);
 };
